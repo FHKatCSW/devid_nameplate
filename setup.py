97bf481ded839c497a6101670b4b3d05df94777a
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-07 18:00:30 +0100

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='devid_nameplate',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='florianhandke',
+    author_email='florian.handke@keyfactor.com',
+    description=''
+)
